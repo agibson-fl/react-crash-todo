03e56b59acd470171ba3d3a67b4cd0de36b30885
agibby5 <agibby5@gmail.com>
2020-02-15 14:48:59 -0500

    add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "./components/layout/Header";
 import "./App.css";
 import Todos from "./components/Todos";
 
@@ -43,6 +44,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos
           todos={this.state.todos}
           toggleComplete={this.toggleComplete}
